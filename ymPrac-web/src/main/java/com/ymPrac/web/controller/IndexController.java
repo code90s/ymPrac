7059d8530a43d6e9f7118c2eb58104b2d6480b6a
Yan Meng <yanmeng@lianjia.com>
2016-09-21 16:20:39 +0800

    添加 Velocity 模板支持

--- ymPrac-web/src/main/java/com/ymPrac/web/controller/IndexController.java
+++ ymPrac-web/src/main/java/com/ymPrac/web/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.ymPrac.web.controller;
 
+import com.google.common.collect.Lists;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Yan Meng on 2016/9/21.
@@ -14,5 +19,20 @@ public class IndexController {
     @ResponseBody
     public String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/hellow")
+    public String hello(Map<String, Object> model){
+        List<String> l= Lists.newArrayList();
+        l.add("哈喽，hadoop");
+        l.add("哈喽，hbase");
+        l.add("哈喽，hive");
+        l.add("哈喽，pig");
+        l.add("哈喽，zookeeper");
+        l.add("哈喽，三劫散仙");
+        //将数据存放map里面，可以直接在velocity页面，使用key访问
+        model.put("data",l);
+
+        return "/hellow";
     }
 }
