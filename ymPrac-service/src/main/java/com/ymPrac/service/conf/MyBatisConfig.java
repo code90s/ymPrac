91299ea94e744c3f0ee6040b0915adc1d11ee753
Yan Meng <yanmeng@lianjia.com>
2016-09-21 21:58:54 +0800

    整合mybatis druid 带完善

---
+++ ymPrac-service/src/main/java/com/ymPrac/service/conf/MyBatisConfig.java
@@ -0,0 +1,54 @@
+package com.ymPrac.service.conf;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * MyBatis基础配置
+ *
+ * @author liuzh
+ * @since 2015-12-19 10:11
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.ymPrac")
+public class MyBatisConfig  {
+
+    @Bean(destroyMethod = "close", initMethod = "init")
+    @Primary
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource createDatasource() {
+        return new DruidDataSource();
+    }
+
+    @Bean(name = "ymPraManager")
+    public PlatformTransactionManager txManager() {
+        return new DataSourceTransactionManager(createDatasource());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(createDatasource());
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        sqlSessionFactoryBean.setMapperLocations(resolver
+                .getResources("classpath*:mybatis/mappers/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+}
---
+++ ymPrac-service/src/main/java/com/ymPrac/service/dao/UserMapper.java
@@ -0,0 +1,9 @@
+package com.ymPrac.service.dao;
+
+/**
+ * Created by Yan Meng on 2016/9/21.
+ */
+public interface UserMapper {
+
+    int countAll();
+}
