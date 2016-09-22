3cf8a7a37a14e64ed1db24b45e427a5c051c4c8d
Yan Meng <yanmeng@lianjia.com>
2016-09-22 20:17:16 +0800

    active mq 连接池

--- ymPrac-mq/src/main/java/com/ymPrac/mq/Producer.java
+++ ymPrac-mq/src/main/java/com/ymPrac/mq/Producer.java
@@ -1,7 +1,6 @@
 package com.ymPrac.mq;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +11,7 @@ import javax.jms.Queue;
  * Created by Yan Meng on 2016/9/22.
  */
 @Component
-public class Producer implements CommandLineRunner {
+public class Producer {
 
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
@@ -20,13 +19,7 @@ public class Producer implements CommandLineRunner {
     @Autowired
     private Queue queue;
 
-    @Override
-    public void run(String... args) throws Exception {
-//        send("Sample message");
-        System.out.println("Message was sent to the Queue");
-    }
-
-    void send(String msg) {
+    public void send(String msg) {
         this.jmsMessagingTemplate.convertAndSend(this.queue, msg);
     }
 }
