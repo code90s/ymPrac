74640b8c10a1755edb57a7aea8588770394fa42f
Yan Meng <yanmeng@lianjia.com>
2017-01-18 18:27:07 +0800

    drools fix

--- ymPrac-drools/src/main/java/com/ymPrac/drools/PointRuleEngine.java
+++ ymPrac-drools/src/main/java/com/ymPrac/drools/PointRuleEngine.java
@@ -10,17 +10,17 @@ public interface PointRuleEngine {
     /**
      * 初始化规则引擎
      */
-    public void initEngine();
+    void initEngine();
 
     /**
      * 刷新规则引擎中的规则
      */
-    public void refreshEnginRule();
+    void refreshEnginRule();
 
     /**
      * 执行规则引擎
      *
      * @param pointDomain 积分Fact
      */
-    public void executeRuleEngine(final PointDomain pointDomain);
+    void executeRuleEngine(final PointDomain pointDomain);
 }
---
+++ ymPrac-drools/src/main/java/com/ymPrac/drools/examples/StringUtils.java
@@ -0,0 +1,13 @@
+package com.ymPrac.drools.examples;
+
+public class StringUtils {
+	public static String getIs() {
+		return "aaaaa:";
+	}
+	
+	public static int getCount() {
+		return 1;
+	}
+	
+	public int getc(){return 1;}
+}
