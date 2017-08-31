9064e2df95e1a2f3be2c25d2ae06436da9d8c14c
Yan Meng <yanmeng@lianjia.com>
2017-08-31 18:45:19 +0800

    add some codee

---
+++ ymPrac-jvm/src/main/java/com/ymPrac/jvm/algorithm/ReBuildTree.java
@@ -0,0 +1,58 @@
+package com.ymPrac.jvm.algorithm;
+
+/**
+ * 二叉树重建
+ * Created by Yan Meng on 2017/8/31.
+ */
+public class ReBuildTree {
+
+    public static void main(String[] args) {
+        int[] pre = new int[]{1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = new int[]{4, 7, 2, 1, 5, 3, 8, 6};
+
+        TreeNode treeNode = reConstructBinaryTree(pre, in);
+        System.out.println(treeNode);
+    }
+
+    private static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        return reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
+    }
+
+    // 前序遍历{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}
+    private static TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
+        if (startPre > endPre || startIn > endIn)
+            return null;
+
+        TreeNode root = new TreeNode(pre[startPre]);
+
+        for (int i = startIn; i <= endIn; i++) {
+            if (in[i] == pre[startPre]) {
+                // startPre + i - startIn
+                root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn, in, startIn, i - 1);
+                // i - startIn + startPre + 1     i-startIn+1
+                root.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+            }
+        }
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+}
