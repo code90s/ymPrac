d03c3519d6983c4225e99119ba4e76fff75a54c4
Yan Meng <yanmeng@lianjia.com>
2017-09-13 12:40:12 +0800

    some test code

--- ymPrac-jvm/src/main/java/com/ymPrac/jvm/test/StringTest.java
+++ ymPrac-jvm/src/main/java/com/ymPrac/jvm/test/StringTest.java
@@ -8,10 +8,37 @@ import java.util.Arrays;
 public class StringTest {
 
     public static void main(String[] args) {
+        if (a() || b()) {
+            System.out.println("==============================");
+        }
+
+
+
         String s = "aaa";
 
         String[] idsArray = s.split(";");
 
         System.out.println(Arrays.toString(idsArray));
+
+        String regEx = "[A-Z,a-z,0-9~！!@#￥$%^&*_+=\uFEFF|/<>：；，.。“‘(）【】{}-]*";
+        String a = "-";
+        boolean matches = a.matches(regEx);
+        try {
+            System.out.println(matches);
+        } catch (Exception e) {
+            System.out.println("error");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+
+    public static boolean a () {
+        System.out.println("aaaaaaaaaaaa");
+        return true;
+    }
+
+    public static boolean b () {
+        System.out.println("bbbbbbbbbbbbbb");
+        return false;
     }
 }
