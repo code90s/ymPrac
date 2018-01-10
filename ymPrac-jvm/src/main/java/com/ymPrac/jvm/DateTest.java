0c17552acec029e43fd4adca699f58d3559780c3
Yan Meng <yanmeng@lianjia.com>
2018-01-10 11:22:25 +0800

    add 尾遞歸

--- ymPrac-jvm/src/main/java/com/ymPrac/jvm/DateTest.java
+++ ymPrac-jvm/src/main/java/com/ymPrac/jvm/DateTest.java
@@ -2,6 +2,9 @@ package com.ymPrac.jvm;
 
 import org.joda.time.DateTime;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Yan Meng on 2017/11/13.
  */
@@ -14,6 +17,27 @@ public class DateTest {
         System.out.println(dt);
         System.out.println(dateTime);
 
+        Set set1 = new HashSet();
+        set1.add("a");
+        set1.add("b");
+        set1.add("d");
+        Set set2 = new HashSet();
+        set2.add("a");
+        set2.add("b");
+        set2.add("c");
+
+        Set sn = new HashSet(set1);
+        System.out.println(sn.retainAll(set2));  //求交集
+        System.out.println(sn);
+
+        Set su = new HashSet(set1);
+        System.out.println(su.addAll(set2));
+        System.out.println(su);//求并集
+
+        Set se = new HashSet();
+        se.add("z");
+        System.out.println(se.retainAll(set1));
+        System.out.println(se);
 
 //        System.out.println(dt.getDayOfMonth());
 //        System.out.println(dt.getHourOfDay());
